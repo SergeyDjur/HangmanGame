8e099f6125f2f64ce3f3925932bcfb10e240dbda
Sergey Djur <sergeydjur@gmail.com>
2026-09-01 10:32:37 +0300

    add RandomWordPicker class and create getRandomWordFromFile method

---
+++ src/RandomWordPicker.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Random;
+
+public class RandomWordPicker {
+    private static final Random RANDOM = new Random();
+    private static final int MIN_WORD_LENGTH = 4;
+    private final static int MAX_WOR_LENGTH = 8;
+
+    public String getRandomWordFromFile(String filePath) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get(filePath));
+        if (lines.isEmpty()) {
+            throw new RuntimeException("file with words is empty");
+        }
+        while (true) {
+            int index = RANDOM.nextInt(lines.size());
+            String word = lines.get(index).trim();
+            if (word.trim().length() < MIN_WORD_LENGTH || word.trim().length() > MAX_WOR_LENGTH) {
+                continue;
+            }
+            return word;
+        }
+    }
+
+}
