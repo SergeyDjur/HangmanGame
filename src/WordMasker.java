f66eee6579e1a989e5152c8ebb4410d41ed0117d
Sergey Djur <sergeydjur@gmail.com>
2026-09-02 14:28:03 +0300

    add WordMasker class and create getMaskedWordMethod

---
+++ src/WordMasker.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+public class WordMasker {
+    RandomWordPicker wordPicker = new RandomWordPicker();
+    private final String word = wordPicker.getRandomWordFromFile("russianNouns.txt");
+
+    public WordMasker() throws IOException {
+
+    }
+
+    public String getMaskedWord() {
+        StringBuilder wordBuilder = new StringBuilder(word.length());
+        wordBuilder.append("*".repeat(word.length()));
+        return wordBuilder.toString();
+    }
+
+}
