7fa3fac5d05e6da1cfb3daf76977dd13b962587a
Sergey Djur <sergeydjur@gmail.com>
2026-09-03 00:03:08 +0300

    Update : add getSymbolFromUser method

--- src/UserMenu.java
+++ src/UserMenu.java
@@ -27,6 +27,26 @@ public class UserMenu {
 
     }
 
-
+    public char getSymbolFromUser() {
+        System.out.println("type a letter");
+        while (true) {
+            String input = scanner.nextLine().trim().toLowerCase();
+            if (input.isEmpty()) {
+                System.out.println("input value cannot be empty, type a  letter");
+                continue;
+            }
+            if (input.length() != 1) {
+                System.out.println("user is allowed to type only one letter,type a single letter");
+                continue;
+            }
+            char symbol = input.toLowerCase().trim().charAt(0);
+            if (!Character.isLetter(symbol)) {
+                System.out.println("only letters allowed, type a single letter");
+                continue;
+            }
+            return symbol;
+        }
+    }
+    
 
 }
