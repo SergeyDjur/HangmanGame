9a223ab0cbd3969f91d0f8737bc6140286f74a5e
Sergey Djur <sergeydjur@gmail.com>
2026-09-07 15:31:38 +0300

    add getLetter method

--- src/UserInput.java
+++ src/UserInput.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class UserInput {
+    private final Scanner scanner;
+
+    public UserInput(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public char getLetter() {
+        System.out.println("type a letter");
+        while (true) {
+            String input = scanner.nextLine().trim().toLowerCase();
+            if (input.isEmpty()) {
+                System.out.println("input value cannot be empty, type a  letter");
+                continue;
+            }
+            if (input.length() != 1) {
+                System.out.println("user is allowed to type only one letter,type a single letter");
+                continue;
+            }
+
+            char symbol = input.toLowerCase().trim().charAt(0);
+            if (!Character.isLetter(symbol)) {
+                System.out.println("only letters allowed, type a single letter");
+                continue;
+            }
+            if (symbol >= 'а' && symbol <= 'я') {
+                return symbol;
+            }
+            System.out.println("only russian language supports, type a russian letter");
+
+        }
+    }
 }
