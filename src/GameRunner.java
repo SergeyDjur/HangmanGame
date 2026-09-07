2dd1e39d5d06fae23e2b2141d4bbc34e242f2b51
Sergey Djur <sergeydjur@gmail.com>
2026-09-07 16:22:15 +0300

    create constructor with parametrs of objects to initialise them inside of constructor

--- src/GameRunner.java
+++ src/GameRunner.java
@@ -1,31 +1,41 @@
-import java.io.IOException;
-
+import java.util.Scanner;
 
 public class GameRunner {
-    WordGuesser guesser = new WordGuesser();
-    UserMenu menu = new UserMenu();
+    Scanner scanner = new Scanner(System.in);
+    WordGuesser guesser;
+    UserMenu menu;
+    RandomWordPicker picker;
+    WordMasker masker;
+    UserInput input;
+    HangmanStages stages;
 
 
-    public GameRunner() throws IOException {
+    public GameRunner() {
+        menu = new UserMenu(scanner);
+        input = new UserInput(scanner);
+        picker = new RandomWordPicker(4, 8);
+        masker = new WordMasker();
+        stages = new HangmanStages();
+        guesser = new WordGuesser(input, picker, masker, stages);
     }
 
     public void runHangman() {
 
         int choice;
         do {
-             choice = menu.askChoice();
+            choice = menu.getCommand();
             if (choice == 1) {
-                guesser.run();
+                guesser.start();
             }
             if (choice == 2) {
                 System.out.println("game is closing");
             }
         } while (choice == 1);
 
-        menu.scanner.close();
+        scanner.close();
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         GameRunner runner = new GameRunner();
         runner.runHangman();
 
