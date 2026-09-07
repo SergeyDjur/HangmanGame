7da1251214c68732d42c0e145b5f42f28199d814
Sergey Djur <sergeydjur@gmail.com>
2026-09-07 18:08:51 +0300

    fix: add parentless in validation method for cyrillic letters

--- src/UserInput.java
+++ src/UserInput.java
@@ -11,10 +11,12 @@ public class UserInput {
         System.out.println("type a letter");
         while (true) {
             String input = scanner.nextLine().trim().toLowerCase();
+
             if (input.isEmpty()) {
                 System.out.println("input value cannot be empty, type a  letter");
                 continue;
             }
+
             if (input.length() != 1) {
                 System.out.println("user is allowed to type only one letter,type a single letter");
                 continue;
@@ -25,7 +27,7 @@ public class UserInput {
                 System.out.println("only letters allowed, type a single letter");
                 continue;
             }
-            if (symbol >= 'а' && symbol <= 'я' || symbol == 'ё') {
+            if ((symbol >= 'а' && symbol <= 'я') || (symbol == 'ё')) {
                 return symbol;
             }
             System.out.println("only russian language supports, type a russian letter");
