d8cb4daec7004bc2888240b32032a4a4a75e5d99
Sergey Djur <sergeydjur@gmail.com>
2026-09-07 16:20:00 +0300

    add method reset and create constructor with parameters

--- src/WordGuesser.java
+++ src/WordGuesser.java
@@ -1,39 +1,86 @@
-import java.io.IOException;
 
+import java.util.HashSet;
+import java.util.Set;
 
 public class WordGuesser {
-    WordMasker masker = new WordMasker();
-
-    HangmanStages printer = new HangmanStages();
-    UserMenu menu = new UserMenu();
-
-    String maskedWord = masker.getMaskedWord();
-
-    String wordToGuess = masker.getWord();
-
-    private final int MAX_AMOUNT_MISTAKES = 6;
-
+    private final WordMasker masker;
+    private final HangmanStages stages;
     private int amountMistakes;
+    private final RandomWordPicker picker;
+    private final UserInput input;
+    private final Set<Character> wrongLetters = new HashSet<>();
+
+    String word;
+    private String maskedWord;
+
+    private static final int MAX_AMOUNT_MISTAKES = 6;
 
 
-    public WordGuesser() throws IOException {
+    public WordGuesser(UserInput input, RandomWordPicker picker, WordMasker masker, HangmanStages stages) {
+        this.input = input;
+        this.picker = picker;
+        this.masker = masker;
+        this.stages = stages;
+
     }
 
-    public boolean isWordContainsLetter(char symbol) {
 
-        for (int i = 0; i < wordToGuess.length(); i++) {
-            if (wordToGuess.charAt(i) == symbol) {
+    public void start() {
+        reset();
+        while (!isGameOver()) {
+            printMaskedWord();
+            stages.printStages(amountMistakes);
+            char symbol = Character.toLowerCase(input.getLetter());
+
+            if (isWordContainsLetter(symbol)) {
+                updateMaskedWord(symbol);
+            } else if (!wrongLetters.contains(symbol)) {
+                wrongLetters.add(symbol);
+                printWrongLetters();
+                amountMistakes++;
+            } else {
+                printWarningMessage();
+            }
+
+            if (isWin()) {
+                printWinMessage();
+            } else if (isLose()) {
+                printLoseMessage();
+                stages.printLoseGameState();
+            }
+
+        }
+
+    }
+
+    public void reset() {
+        wrongLetters.clear();
+        amountMistakes = 0;
+        word = picker.getRandomWordFromFile("russianNouns.txt");
+        maskedWord = masker.getMaskedWord(word);
+    }
+public void printWrongLetters() {
+    System.out.println("wrong letters : ");
+        for(char c : wrongLetters) {
+            System.out.println(c);
+        }
+}
+    private boolean isWordContainsLetter(char symbol) {
+
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == symbol) {
                 return true;
             }
         }
         return false;
     }
 
-    public void updateMaskedWord(char symbol) {
+
+    private void updateMaskedWord(char symbol) {
         StringBuilder updatedMaskedWord = new StringBuilder();
 
-        for (int i = 0; i < wordToGuess.toLowerCase().length(); i++) {
-            if (wordToGuess.toLowerCase().charAt(i) == symbol) {
+        for (int i = 0; i < word.length(); i++) {
+            if (word.toLowerCase().charAt(i) == symbol) {
                 updatedMaskedWord.append(symbol);
             } else {
                 updatedMaskedWord.append(maskedWord.charAt(i));
@@ -42,34 +89,35 @@ public class WordGuesser {
         }
         maskedWord = updatedMaskedWord.toString();
 
+    }
 
+    private void printWarningMessage() {
+        System.out.println("you've already typed this letter ,you cant do it twice");
     }
 
 
-    public void run() {
+    private void printMaskedWord() {
+        System.out.println(maskedWord);
+    }
 
-        while (true) {
-            System.out.println(maskedWord);
-            printer.print(amountMistakes);
-            char symbol = Character.toLowerCase(menu.getSymbolFromUser());
-            if (isWordContainsLetter(symbol)) {
-                updateMaskedWord(symbol);
-            } else {
-                amountMistakes++;
-                printer.print(amountMistakes);
-            }
-            if (!maskedWord.contains("*")) {
-                System.out.println("you win");
-                break;
-            }
-            if (amountMistakes == MAX_AMOUNT_MISTAKES) {
-                System.out.println("you lost");
-                System.out.println("word you tried to guess is " + wordToGuess);
-                break;
-            }
+    private boolean isWin() {
+        return !maskedWord.contains("*");
+    }
 
-        }
+    private boolean isLose() {
+        return amountMistakes >= MAX_AMOUNT_MISTAKES;
+    }
 
+    private void printWinMessage() {
+        System.out.println("you win");
+    }
+
+    private void printLoseMessage() {
+        System.out.println("you lose, word you tried to guess is " + word);
+    }
+
+    private boolean isGameOver() {
+        return isWin() || isLose();
     }
 
 }
