1cacc18325949d69b2c26db9560aabf0be6cbdd5
Sergey Djur <sergeydjur@gmail.com>
2026-09-07 16:17:53 +0300

    simplify method getMaskedWord and remove variable word and remove method getWord

--- src/WordMasker.java
+++ src/WordMasker.java
@@ -1,20 +1,8 @@
-import java.io.IOException;
 
 public class WordMasker {
-    RandomWordPicker wordPicker = new RandomWordPicker();
-    private final String word = wordPicker.getRandomWordFromFile("russianNouns.txt");
-
-    public WordMasker() throws IOException {
-
-    }
-    public String getWord() {
-        return word;
+    public String getMaskedWord(String word) {
+        return "*".repeat(word.length());
     }
 
-    public String getMaskedWord() {
-        StringBuilder wordBuilder = new StringBuilder(word.length());
-        wordBuilder.append("*".repeat(word.length()));
-        return wordBuilder.toString();
-    }
 
 }
