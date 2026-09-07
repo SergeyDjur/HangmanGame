b7499c411a51d5ce3cb48268568e9af96bfac9ac
Sergey Djur <sergeydjur@gmail.com>
2026-09-07 15:40:45 +0300

    change access modifier in []STAGES from public to private static final.Rename method print.Create method printLoseGameState.Rename stagesOfHangman array

--- src/GameRunner.java
+++ src/GameRunner.java
@@ -1,31 +1,47 @@
-import java.io.IOException;
-
+import java.util.Scanner;
 
 public class GameRunner {
-    WordGuesser guesser = new WordGuesser();
-    UserMenu menu = new UserMenu();
+
+    private final Scanner scanner;
+    private final UserMenu menu;
+    private final WordGuesser guesser;
 
 
-    public GameRunner() throws IOException {
+    public GameRunner() {
+        scanner = new Scanner(System.in);
+        menu = new UserMenu(scanner);
+        UserInput input = new UserInput(scanner);
+        RandomWordPicker picker = new RandomWordPicker(4, 8);
+        WordMasker masker = new WordMasker();
+        HangmanStages stages = new HangmanStages();
+        guesser = new WordGuesser(input, picker, masker, stages);
+
     }
+
 
     public void runHangman() {
 
         int choice;
         do {
-             choice = menu.askChoice();
+            choice = menu.getCommand();
             if (choice == 1) {
-                guesser.run();
+                guesser.start();
+            } else if (choice == 2) {
+                printCloseGameMessage();
             }
-            if (choice == 2) {
-                System.out.println("game is closing");
-            }
-        } while (choice == 1);
 
-        menu.scanner.close();
+
+        } while (choice != 2);
+
+        scanner.close();
     }
 
-    public static void main(String[] args) throws IOException {
+
+    private void printCloseGameMessage() {
+        System.out.println("game is closing");
+    }
+
+    public static void main(String[] args) {
         GameRunner runner = new GameRunner();
         runner.runHangman();
 
