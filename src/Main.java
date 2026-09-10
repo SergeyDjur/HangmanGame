32c360f6c43f3f30a96ea562d7387c0a9aab6c0f
Sergey Djur <sergeydjur@gmail.com>
2026-09-10 23:57:20 +0300

    add main class with method main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        UserMenu menu = new UserMenu(scanner);
+        UserInput input = new UserInput(scanner);
+        RandomWordPicker picker = new RandomWordPicker(4, 8);
+        WordMasker masker = new WordMasker();
+        HangmanStages stages = new HangmanStages();
+
+        WordGuesser guesser = new WordGuesser(input, picker, masker, stages);
+        GameRunner runner = new GameRunner(menu, guesser);
+        runner.runHangman();
+    }
 }
